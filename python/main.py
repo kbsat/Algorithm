9836e97f0b55729cd5554fca52b5d847eab42e62
unknown <kbsat@naver.com>
2020-09-05 21:45:04 +0900

    백준 2741

--- python/main.py
+++ python/main.py
@@ -1,23 +1,7 @@
-# 백준 : 2884
-# 알람시계
+# 백준 : 2741
+# N 찍기
 
-# 입력한 시, 분 보다 45분 일찍 알람 설정하기
+n = int(input())
 
-# hour = int(input())
-# minute = int(input())
-hour, minute = input().split()
-hour = int(hour)
-minute = int(minute)
-
-# 0<=H<=23 ,0<=M<=59
-
-if minute >= 45:
-    minute -= 45
-else:
-    minute = minute-45+60
-    if hour == 0:
-        hour = 23
-    else:
-        hour -= 1
-
-print(str(hour), str(minute))
+for i in range(n):
+    print(i+1)
