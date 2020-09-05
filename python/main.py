1cfcef913064d11287e2c624debe55cec44e5b88
unknown <kbsat@naver.com>
2020-09-05 21:54:16 +0900

    백준 11021

--- python/main.py
+++ python/main.py
@@ -1,8 +1,13 @@
-# 백준 : 2742
-# 기찍 N
+# 백준 : 11021
+#  A+B-7
 
-n = int(input())
+T = int(input())
+answer = []
+for i in range(T):
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    answer.append(a+b)
 
-for i in range(n, 0, -1):
-    print(i)
-#  same : for( int i = 5 ; i > 0 ; i--)
+for i in range(T):
+    print("Case #"+str(i+1)+": "+str(answer[i]))
