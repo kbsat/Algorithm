4ee49016af16e93694f77e77b7547d4aa0bf1fcf
unknown <kbsat@naver.com>
2020-09-05 21:43:08 +0900

    백준 2884

--- python/main.py
+++ python/main.py
@@ -1,11 +1,23 @@
-x = int(input())
-y = int(input())
+# 백준 : 2884
+# 알람시계
 
-if x > 0 and y > 0:  # 1사분면
-    print(str(1))
-elif x < 0 and y > 0:
-    print(str(2))
-elif x < 0 and y < 0:
-    print(str(3))
+# 입력한 시, 분 보다 45분 일찍 알람 설정하기
+
+# hour = int(input())
+# minute = int(input())
+hour, minute = input().split()
+hour = int(hour)
+minute = int(minute)
+
+# 0<=H<=23 ,0<=M<=59
+
+if minute >= 45:
+    minute -= 45
 else:
-    print(str(4))
+    minute = minute-45+60
+    if hour == 0:
+        hour = 23
+    else:
+        hour -= 1
+
+print(str(hour), str(minute))
