e7b6e3778bfda32ac1560af2fe0fe47449934dc1
unknown <kbsat@naver.com>
2020-09-05 22:06:20 +0900

    백준 2439

--- python/main.py
+++ python/main.py
@@ -1,9 +1,14 @@
-# 백준 : 2438
-#  별찍기 - 1
+# 백준 : 2439
+#  별찍기 - 2
 
 n = int(input())
+space_num = n
+for i in range(n):  # n번 반복해줌
 
-for i in range(n):
-    for j in range(i+1):
+    # n-1 ~ 0개의 공백을 찍어줌
+    for j in range(space_num-1, 0, -1):
+        print(" ", end="")
+    for k in range(i+1):  # 1~ n개의 별을 찍어줌
         print("*", end="")
     print()
+    space_num -= 1
