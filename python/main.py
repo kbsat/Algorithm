8c3eb052688dc1956983489ad2bfb55bf4e01dcd
kbsat <kbsat@naver.com>
2020-09-18 20:47:31 +0900

    백준 1110

--- python/main.py
+++ python/main.py
@@ -1,9 +1,19 @@
-# 백준 : 10951
-# A+B-4
+# 백준 : 1110
+# 더하기 사이클
 
-try:
-    while True:
-        a, b = map(int, input().split())
-        print(a+b)
-except:
-    exit()
+n = int(input())  # 26
+a_origin = int(n / 10)  # 2
+b_origin = int(n % 10)  # 6
+i = 0
+
+a = a_origin
+b = b_origin
+while True:
+    i += 1
+    c = (a + b) % 10
+    a = b
+    b = c
+    if a == a_origin and b == b_origin:
+        break
+
+print(i)
