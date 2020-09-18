a6465d1880bb1004f9b8cc2aaf52cfa9b9528360
kbsat <kbsat@naver.com>
2020-09-18 17:15:19 +0900

    백준 10952

--- python/main.py
+++ python/main.py
@@ -1,15 +1,11 @@
-# 백준 : 10871
-# X보다 작은 수
+# 백준 : 10952
+# A+B-5
 
-n, x = input().split()
-n = int(n)
-x = int(x)
-small_nums = []
-nums = input().split()
-
-for num in nums:
-    if int(num) < x:
-        small_nums.append(num)
-
-for small_num in small_nums:
-    print(small_num, end=" ")
+a, b = input().split()
+a = int(a)
+b = int(b)
+while a != 0 and b != 0:
+    print(a+b)
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
