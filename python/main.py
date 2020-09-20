dbc740528981ae7989d6412dd8d7924c008d7c69
unknown <kbsat@naver.com>
2020-09-20 22:36:25 +0900

    백준 15596

--- python/main.py
+++ python/main.py
@@ -1,21 +1,8 @@
-# 백준 : 4344
-# 평균은 넘겠지
+# 백준 :15596
+# 정수 N개의 합
 
-C = int(input())
-answer_list = []
-for i in range(C):
-    score_list = list(map(int, input().split()))
-    student_number = score_list[0]
-    score_list.remove(student_number)
-    average = sum(score_list) / student_number
-
-    higher_average_num = 0
-    for score in score_list:
-        if score > average:
-            higher_average_num += 1
-
-    higher_average_percent = (higher_average_num / student_number) * 100
-    answer_list.append(higher_average_percent)
-
-for answer in answer_list:
-    print(format(answer, ".3f")+"%")
+def solve(a):
+    sum = 0
+    for a_element in a:
+        sum += a_element
+    return sum
