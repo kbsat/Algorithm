8992b3441752082be3216af08fc1ecf5d220f837
kbsat <kbsat@naver.com>
2020-09-25 00:41:01 +0900

    백준 10757

--- python/main.py
+++ python/main.py
@@ -1,17 +1,5 @@
-# 백준 : 10170
-# NFC West vs North
+# 백준 : 10757
+# 큰 수 A+B
 
-print("\
-NFC West       W   L  T\n\
------------------------\n\
-Seattle        13  3  0\n\
-San Francisco  12  4  0\n\
-Arizona        10  6  0\n\
-St. Louis      7   9  0\n\
-\n\
-NFC North      W   L  T\n\
------------------------\n\
-Green Bay      8   7  1\n\
-Chicago        8   8  0\n\
-Detroit        7   9  0\n\
-Minnesota      5  10  1")
+A, B = map(int, input().split())
+print(A+B)
