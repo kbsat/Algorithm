82a3849d16ce5aed5c5c14f7a6b16e923f5c3a57
kbsat <kbsat@naver.com>
2020-09-24 22:55:38 +0900

    백준 1271

--- python/main.py
+++ python/main.py
@@ -1,20 +1,9 @@
-# 백준 : 1065
-# 한수
+# 백준 : 1271
+# 엄청난 부자2
 
-def isHan(x):
-    target = list(map(int, str(x)))
-    if x <= 99:
-        return True
-    elif target[0] - target[1] == target[1] - target[2]:
-        return True
+n, m = map(int, input().split())
 
-    return False
-
-
-num = int(input())
-result = 0
-for i in range(1, num+1):
-    if isHan(i):
-        result += 1
-
-print(result)
+distribution = n // m
+remainder = int(n % m)
+print(str(distribution))
+print(str(remainder))
