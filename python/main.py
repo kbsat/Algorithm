8331b8f0ea49416a67f050949bd80dda019db577
kbsat <kbsat@naver.com>
2020-09-24 23:27:32 +0900

    백준 2558

--- python/main.py
+++ python/main.py
@@ -1,11 +1,6 @@
-# 백준 : 2475
-# 검증수
+# 백준 : 2558
+# A+B -2
 
-# 5자리 숫자를 받아서 자리수별로 리스트형성
-id = list(map(int, input().split()))
-
-sum = 0
-for id_num in id:
-    sum += (id_num*id_num)
-verify_num = sum % 10
-print(verify_num)
+A = int(input())
+B = int(input())
+print(str(A+B))
