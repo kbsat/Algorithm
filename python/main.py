ad51225933a327ed2bfebcfe727181ba676292f1
kbsat <kbsat@naver.com>
2020-11-04 17:48:56 +0900

    프로그래머스 : 두 개 뽑아서 더하기 ( set, sorted 함수 이용 )

--- python/main.py
+++ python/main.py
@@ -2,16 +2,11 @@
 # 두 개 뽑아서 더하기
 def solution(numbers):
     answer = []
-    sortedNumbers = numbers
-    sortedNumbers.sort()
-    sum = 0
-    for i in range(len(sortedNumbers)):
-        for j in range(i+1, len(sortedNumbers)):
-            sum = sortedNumbers[i] + sortedNumbers[j]
-            if sum not in answer:
-                answer.append(sum)
-    answer.sort()
-    return answer
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            answer.append(numbers[i]+numbers[j])
+
+    return sorted(list(set(answer)))
 
 
 numbers = [2, 1, 3, 4, 1]
