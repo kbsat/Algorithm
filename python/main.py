215da81dad6d9428ac66c6bbb9d4ddd27ef08c11
kbsat <kbsat@naver.com>
2020-11-04 17:46:32 +0900

    프로그래머스 : 두 개 뽑아서 더하기 ( list만 이용 )

--- python/main.py
+++ python/main.py
@@ -1,26 +1,18 @@
 # programmers
-# 크레인인형뽑기
-def solution(board, moves):
-    result = []
-    answer = 0
-    for move in moves:
-        for i in board:
-            if i[move-1] != 0:
-                result.append(i[move-1])
-                i[move-1] = 0
-                break
-        if len(result) >= 2 and result[-1] == result[-2]:
-            result.pop()
-            result.pop()
-            answer += 2
+# 두 개 뽑아서 더하기
+def solution(numbers):
+    answer = []
+    sortedNumbers = numbers
+    sortedNumbers.sort()
+    sum = 0
+    for i in range(len(sortedNumbers)):
+        for j in range(i+1, len(sortedNumbers)):
+            sum = sortedNumbers[i] + sortedNumbers[j]
+            if sum not in answer:
+                answer.append(sum)
+    answer.sort()
     return answer
 
 
-board = [[0, 0, 0, 0, 0],
-         [0, 0, 1, 0, 3],
-         [0, 2, 5, 0, 1],
-         [4, 2, 4, 4, 2],
-         [3, 5, 1, 3, 1]]
-moves = [1, 5, 3, 5, 1, 2, 1, 4]
-
-solution(board, moves)
+numbers = [2, 1, 3, 4, 1]
+solution(numbers)
