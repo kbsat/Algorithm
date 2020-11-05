ac7ce9d58bf7b1843b8af2142a415f01ed95dc2b
kbsat <kbsat@naver.com>
2020-11-05 22:04:03 +0900

    프로그래머스 : 체육복

--- python/main.py
+++ python/main.py
@@ -1,24 +1,19 @@
 # 프로그래머스
-# 모의고사
+# 체육복
 
-def solution(answers):
-    answer = []
-    scores = [0, 0, 0]
-    first_supoja = [1, 2, 3, 4, 5]
-    second_supoja = [2, 1, 2, 3, 2, 4, 2, 5]
-    third_supoja = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    for enum, ans in enumerate(answers):
-        if first_supoja[enum % len(first_supoja)] == ans:
-            scores[0] += 1
-        if second_supoja[enum % len(second_supoja)] == ans:
-            scores[1] += 1
-        if third_supoja[enum % len(third_supoja)] == ans:
-            scores[2] += 1
+def solution(n, lost, reserve):
+    new_lost = list(set(lost) - set(reserve))
+    new_reserve = list(set(reserve) - set(lost))
+    answer = n - len(new_lost)
 
-    for i in range(len(scores)):
-        if max(scores) == scores[i]:
-            answer.append(int(i+1))
+    for lost_one in new_lost:
+        if new_reserve.count(lost_one-1) > 0:
+            new_reserve.remove(lost_one-1)
+            answer += 1
+        elif new_reserve.count(lost_one+1) > 0:
+            new_reserve.remove(lost_one+1)
+            answer += 1
     return answer
 
 
-solution([1, 3, 2, 4, 2])
+print(solution(5, [1, 2, 3], [1, 2, 3]))
