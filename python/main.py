db2fc6acad472e53df4adbaeed762d0a7fd10e28
kbsat <kbsat@naver.com>
2020-11-06 20:01:44 +0900

    프로그래머스 : 스킬트리

--- python/main.py
+++ python/main.py
@@ -1,17 +1,26 @@
 # 프로그래머스
-# 2016년
+# 스킬트리
 
-def solution(a, b):
-    answer = ''
-    week = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU']
-    dayNum = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    date = 0
-    for i in range(a-1):
-        date += dayNum[i]
-    date += b-1
-    answer = week[(date % 7)]
+def solution(skill, skill_trees):
+    answer = 0
+    check_num = 0
+
+    for tree in skill_trees:
+        for i in range(len(tree)):
+            if tree[i] in skill and check_num != -1:
+                if skill.find(tree[i]) == check_num:
+                    check_num += 1
+                else:
+                    check_num = -1
+
+        if check_num != -1:
+            answer += 1
+
+        check_num = 0
 
     return answer
 
 
-print(solution(1, 1))
+skill = "CBD"
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+print(solution(skill, skill_trees))
