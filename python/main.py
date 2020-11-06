d45eb83b87f87caa28c34239104f23755a6b1d17
kbsat <kbsat@naver.com>
2020-11-06 22:53:18 +0900

    프로그래머스 : 가운데 글자 가져오기

--- python/main.py
+++ python/main.py
@@ -1,26 +1,17 @@
 # 프로그래머스
-# 스킬트리
+# 가운데 글자 가져오기
 
-def solution(skill, skill_trees):
-    answer = 0
-    check_num = 0
-
-    for tree in skill_trees:
-        for i in range(len(tree)):
-            if tree[i] in skill and check_num != -1:
-                if skill.find(tree[i]) == check_num:
-                    check_num += 1
-                else:
-                    check_num = -1
-
-        if check_num != -1:
-            answer += 1
-
-        check_num = 0
-
+def solution(s):
+    answer = ''
+    n = len(s)
+    if n % 2 == 0:
+        answer = s[n//2-1] + s[n//2]
+    else:
+        answer = s[n//2]
     return answer
 
 
-skill = "CBD"
-skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
-print(solution(skill, skill_trees))
+s = "abcde"
+print(s[3])
+
+print(solution(s))
