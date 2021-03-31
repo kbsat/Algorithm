a656be389651ae6e8ddf2dfd4ae15dc4120893cc
BeomsooKim <kbsat@naver.com>
2021-04-01 00:52:32 +0900

    이코테 3. 숫자카드 게임

--- python/main.py
+++ python/main.py
@@ -1,15 +1,20 @@
-# 프로그래머스
-# 두 정수 사이의 합
+# 이코테
+# 3. 숫자 카드 게임
 
-def solution(a, b):
-    answer = 0
-    if a > b:
-        tmp = a
-        a = b
-        b = tmp
+n,m = map(int,input().split())
+cards = [[0] * m for _ in range(n)]
+min_set = set([])
 
-    i = a
-    while i <= b:
-        answer += i
-        i += 1
-    return answer
+# 배열입력
+for i in range(n):
+    cards[i] = list(map(int,input().split()))
+
+print(cards)
+for i in range(n):
+    min_num = cards[i][0]
+    for j in range(m):
+        if min_num > cards[i][j]:
+            min_num = cards[i][j]
+    min_set.add(min_num)
+
+print(max(min_set))
