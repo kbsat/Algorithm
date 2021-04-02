3dec117c8e5ceb373c21c30e0fa6dc1a9d7c68ac
BeomsooKim <kbsat@naver.com>
2021-04-03 00:20:33 +0900

    이코테 4-3. 왕실의 나이트

--- python/main.py
+++ python/main.py
@@ -1,14 +1,18 @@
 # 이코테
-# 예제4-2. 시각
+# 4-3. 왕실의 나이트
 
-n = int(input())
+input_data = input()
+row = int(input_data[1])
+col = int(ord(input_data[0]) - ord('a')) + 1
+
+steps = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
+
 result = 0
+for step in steps:
+    nextRow = row + step[0]
+    nextCol = col + step[1]
 
-for h in range(n+1):
-    for m in range(60):
-        for s in range(60):
-            if '3' in str(h) + str(m) + str(s):
-                result += 1
-
+    if nextRow >= 1 and nextCol >= 1 and nextRow <= 8 and nextCol <= 8:
+        result += 1
 
 print(result)
