581b119551fa92d14d6ee6fe31b3abf4ba974699
BeomsooKim <kbsat@naver.com>
2021-04-05 23:39:55 +0900

    백준 15740. A+B-9

--- python/main.py
+++ python/main.py
@@ -1,18 +1,6 @@
-# 이코테
-# 4-3. 왕실의 나이트
+# 백준
+# 15740. A+B-9
 
-input_data = input()
-row = int(input_data[1])
-col = int(ord(input_data[0]) - ord('a')) + 1
+a,b = map(int,input().split())
 
-steps = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(-1,2),(1,-2),(-1,-2)]
-
-result = 0
-for step in steps:
-    nextRow = row + step[0]
-    nextCol = col + step[1]
-
-    if nextRow >= 1 and nextCol >= 1 and nextRow <= 8 and nextCol <= 8:
-        result += 1
-
-print(result)
+print(a+b)
