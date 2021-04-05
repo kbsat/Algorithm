a820caf8e70a0551c304fd4ca4c12b7d2705621e
BeomsooKim <kbsat@naver.com>
2021-04-05 23:59:21 +0900

    백준 2480. 주사위 세 개

--- python/main.py
+++ python/main.py
@@ -1,6 +1,24 @@
 # 백준
-# 15740. A+B-9
+# 2480. 주사위 세개
 
-a,b = map(int,input().split())
+a,b,c = map(int,input().split())
+aList = [a,b,c]
+dic = {}
+result = 0
 
-print(a+b)
+for num in aList:
+    if num in dic:
+        dic[num] += 1
+    else:
+        dic[num] = 1
+
+if len(dic) == 1:
+    result = dic.keys[0] * 1000 + 10000
+elif len(dic) == 2:
+    for k, v in dic.items():
+        if v == 2:
+            result = k * 100 + 1000
+else:
+    result = max(aList) * 100
+
+print(result)
