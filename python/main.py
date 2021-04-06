7b1edbae5b905a31e940aaeff5439bc7cfd5dc6d
kbsat <kbsat@naver.com>
2021-04-06 20:02:01 +0900

    이코테 5-11. 미로탈출

--- python/main.py
+++ python/main.py
@@ -1,47 +1,42 @@
 # 이코테
-# 5-10. 음료수 얼려 먹기
+# 5-11. 미로탈출
+
+from collections import deque
 
 n, m = map(int,input().split())
 arr = []
-
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 for i in range(n):
     row = list(map(int,input()))
     arr.append(row)
 
+def bfs(x,y):
+    global count
+    q = deque()
+    q.append((x,y))
     
-visited = [ [False] * m for _ in range(n)]
+    while q:
+        x, y = q.popleft()
+    
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m :
+                continue
+            
+            if arr[nx][ny] == 0:
+                continue
 
-def dfs(graph, x, y, visited):
-    n = len(graph)
-    m = len(graph[0])
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = arr[x][y] + 1
+                q.append((nx,ny))
+        
     
-    # 만약 범위를 벗어난 x,y를 골랐다면 중단
-    if x < 0 or y < 0 or x >= n or y >= m:
-        return False
-    
-    if graph[x][y] == 0 and visited[x][y] == False:
-        visited[x][y] = True
-        # 상 하 좌 우 순
-        dfs(graph,x-1,y,visited)
-        dfs(graph,x+1,y,visited)
-        dfs(graph,x,y-1,visited)
-        dfs(graph,x,y+1,visited)
-        return True
-    else:
-        return False
-
-result = 0
-for i in range(n):
-    for j in range(m):
-        if dfs(arr,i,j,visited) == True:
-            result += 1
-
-print(result)
-    
-    
-    
+    return arr[n-1][m-1]
 
 
-    
-    
-    
+
+print(bfs(0,0))
+print(arr)
+
