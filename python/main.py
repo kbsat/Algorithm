94c1872e30c5eb489419238658aa2b4380489691
BeomsooKim <kbsat@naver.com>
2021-04-07 23:44:06 +0900

    백준 1012 . 유기농 배추

--- python/main.py
+++ python/main.py
@@ -1,42 +1,53 @@
-# 이코테
-# 5-11. 미로탈출
+# 백준 - 그래프이론
+# 1012. 유기농 배추
 
-from collections import deque
+testNum = int(input())
+dx = [-1,1,0,0]
+dy = [0,0,1,-1]
+result = [ 0 for _ in range(testNum)]
 
-n, m = map(int,input().split())
-arr = []
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-for i in range(n):
-    row = list(map(int,input()))
-    arr.append(row)
+def dfs(graph, x, y):
+    rowNum = len(graph)
+    colNum = len(graph[0])
 
-def bfs(x,y):
-    global count
-    q = deque()
-    q.append((x,y))
+    if graph[x][y] == 0:
+        return False
     
-    while q:
-        x, y = q.popleft()
+    for i in range(4):
+        nx = x
+        ny = y
+
+        nx += dx[i]
+        ny += dy[i]
+
+        if nx < 0 or ny < 0 or nx >= rowNum or ny >= colNum:
+
+            continue
+        if graph[nx][ny] == 1 :
+            graph[nx][ny] = -1
+        elif graph[nx][ny] == 0:
+            continue
     
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m :
-                continue
-            
-            if arr[nx][ny] == 0:
-                continue
-
-            if arr[nx][ny] == 1:
-                arr[nx][ny] = arr[x][y] + 1
-                q.append((nx,ny))
-        
+    if graph[x][y] == -1:
+        return False
     
-    return arr[n-1][m-1]
+    graph[x][y] = -1
+    return True
+    
 
+for t in range(testNum):
+    m, n, k = map(int,input().split())
+    farm = [[0]*m for _ in range(n)]
+    for _ in range(k):
+        x, y = map(int,input().split())
+        farm[y][x] = 1
+    
 
+    for i in range(n):
+        for j in range(m):
+            if dfs(farm, i, j):
+                result[t] += 1
+    
 
-print(bfs(0,0))
-print(arr)
-
+for res in result:
+    print(res)
