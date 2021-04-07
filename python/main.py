8a86d63857821236a9c9c2404740a5bc50f06dac
BeomsooKim <kbsat@naver.com>
2021-04-08 00:49:02 +0900

    백준 1012. 유기농 배추 - 반례(십자가모양일 때) 해결

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 # 백준 - 그래프이론
 # 1012. 유기농 배추
+import sys
 
 testNum = int(input())
 dx = [-1,1,0,0]
@@ -10,29 +11,47 @@ def dfs(graph, x, y):
     rowNum = len(graph)
     colNum = len(graph[0])
 
-    if graph[x][y] == 0:
+    if x < 0 or y < 0 or x >= rowNum or y >= colNum:
         return False
     
-    for i in range(4):
-        nx = x
-        ny = y
+    if graph[x][y] == 1:
+        graph[x][y] = -1
 
-        nx += dx[i]
-        ny += dy[i]
+        dfs(graph,x+1,y)
+        dfs(graph,x-1,y)
+        dfs(graph,x,y+1)
+        dfs(graph,x,y-1)
+        # for i in range(4):
+        #     nx = x + dx[i]
+        #     ny = y + dy[i]
 
-        if nx < 0 or ny < 0 or nx >= rowNum or ny >= colNum:
-
-            continue
-        if graph[nx][ny] == 1 :
-            graph[nx][ny] = -1
-        elif graph[nx][ny] == 0:
-            continue
+        #     dfs(graph,nx,ny)
+        
+        return True
+    return False
+    # if graph[x][y] == 0:
+    #     return False
     
-    if graph[x][y] == -1:
-        return False
+    # for i in range(4):
+    #     nx = x
+    #     ny = y
+
+    #     nx += dx[i]
+    #     ny += dy[i]
+
+    #     if nx < 0 or ny < 0 or nx >= rowNum or ny >= colNum:
+
+    #         continue
+    #     if graph[nx][ny] == 1 :
+    #         graph[nx][ny] = -1
+    #     elif graph[nx][ny] == 0:
+    #         continue
     
-    graph[x][y] = -1
-    return True
+    # if graph[x][y] == -1:
+    #     return False
+    
+    # graph[x][y] = -1
+    # return True
     
 
 for t in range(testNum):
@@ -47,7 +66,8 @@ for t in range(testNum):
         for j in range(m):
             if dfs(farm, i, j):
                 result[t] += 1
-    
+
+    farm = []
 
 for res in result:
     print(res)
